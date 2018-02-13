6ca83b6eb08f33d6707e6b1e8adadb17e6963067
Poonam <pbharde@gmail.com>
2018-02-13 11:58:16 -0500

    App.js Conflict

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,15 +8,11 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import MainApp from 'routes/app/'
 import Page404 from 'routes/404/'
 import Page500 from 'routes/500/'
-// import PageConfirmEmail from 'routes/confirm-email/'
-// import PageForgotPassword from 'routes/forgot-password/'
+import PageConfirmEmail from 'routes/confirm-email/'
+import PageForgotPassword from 'routes/forgot-password/'
 import PageFullscreen from 'routes/fullscreen/'
 import PageLockScreen from 'routes/lock-screen/'
 import PageLogin from 'routes/login/'
-<<<<<<< HEAD
-// import PageSignUp from 'routes/sign-up/'
-
-=======
 import PageWelcome from 'routes/welcome/'
 import PageRegister from 'routes/register/'
 import PageRegister1 from 'routes/register1/'
@@ -34,7 +30,6 @@ import PageEditPhoneNumber from 'routes/editPhoneNumber/'
 import PageVerifyPhoneCode from 'routes/verifyPhoneCode/'
 import PageMainLogin from 'routes/mainLogin/'
 import PageMotionDetection from 'routes/motion-Detection/'
->>>>>>> pbharde
 // = styles =
 // 3rd
 import 'styles/bootstrap.scss';
@@ -69,7 +64,7 @@ class App extends Component {
 
     const isRoot = location.pathname === '/' ? true : false;
     if (isRoot) {
-      return ( <Redirect to={'/app/home'}/> );
+      return ( <Redirect to={'/app/dashboard'}/> );
     }
 
     return (
@@ -90,14 +85,11 @@ class App extends Component {
             <Route path={`${match.url}app`} component={MainApp} />
             <Route exact path="/404" component={Page404} />
             <Route exact path="/500" component={Page500} />
-            {/* <Route exact path="/confirm-email" component={PageConfirmEmail} /> */}
-            {/* <Route exact path="/forgot-password" component={PageForgotPassword} /> */}
+            <Route exact path="/confirm-email" component={PageConfirmEmail} />
+            <Route exact path="/forgot-password" component={PageForgotPassword} />
             <Route exact path="/fullscreen" component={PageFullscreen} />
             <Route exact path="/lock-screen" component={PageLockScreen} />
             <Route exact path="/login" component={PageLogin} />
-<<<<<<< HEAD
-            {/* <Route exact path="/sign-up" component={PageSignUp} /> */}
-=======
             <Route exact path="/welcome" component={PageWelcome} />
             <Route exact path="/register" component={PageRegister} />
             <Route exact path="/register1" component={PageRegister1} />
@@ -115,7 +107,6 @@ class App extends Component {
             <Route exact path="/verifyPhoneCode" component={PageVerifyPhoneCode} />
             <Route exact path="/mainLogin" component={PageMainLogin} />
             <Route exact path="/motionDetection" component={PageMotionDetection} />
->>>>>>> pbharde
           </div>
         </div>
       </MuiThemeProvider>
