cdc11601356baa9ac9e68234559d6cc884050106
HOWL <devwolf1@HOWLs-iMac.fios-router.home>
2018-05-07 14:27:58 -0400

    SpotCrime data update

--- src/routes/app/routes/map/components/MapClass.js
+++ src/routes/app/routes/map/components/MapClass.js
@@ -2,32 +2,36 @@
 import React from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper, Polygon} from 'google-maps-react';
 import cookie from 'react-cookies';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
-
-// const GOOGLE_MAPS_JS_API_KEY='AIzaSyAATCBLAB6FKMqK0HZMpt75zPQZVM9H4U4';
+ const GOOGLE_MAPS_JS_API_KEY='AIzaSyAATCBLAB6FKMqK0HZMpt75zPQZVM9H4U4';
 
 
 class GoogleMap extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
         zoom: 15,
         showingInfoWindow: false,
         activeMarker: {},
         selectedPlace: {},
+        GetSpotCrimesResult:'',
         lat1: '',
-        log1: '',
+        lon1: ''
 
     }
     this.onMapClicked = this.onMapClicked.bind(this);
     this.onMarkerClicked = this.onMarkerClicked.bind(this);
     this.handleMapMount = this.handleMapMount.bind(this);
+    this.centerMoved = this.centerMoved.bind(this);
 
 }
 
 
-onMapClicked (props) {
+
+onMapClicked (e) {
+
 
     if (this.state.showingInfoWindow) {
         this.setState({
@@ -38,22 +42,51 @@ onMapClicked (props) {
         })
 
     }
-      // console.log("In onclick");
-      const location = window.navigator && window.navigator.geolocation
-
-      if (location) {
-        location.getCurrentPosition((position) => {
-          this.setState({
-            lat1: position.coords.latitude,
-            lon1: position.coords.longitude,
-          })
-        }, (error) => {
-          this.setState({ lat1: 'err-latitude', log1: 'err-longitude' })
-        })
-      }
-   // console.log(this.state.lat1);
-   // console.log(this.state.log1);
-
+   //    console.log("In onclick");
+   //    const location = window.navigator && window.navigator.geolocation
+   //
+   //    if (location) {
+   //
+   //      location.getCurrentPosition((position) => {
+   //        this.setState({
+   //          lat1: position.coords.latitude,
+   //          lon1: position.coords.longitude,
+   //        })
+   //        console.log(this.state.lat1);
+   //        console.log(this.state.lon1);
+   //
+   //        fetch('https://service.howlalarm.com/HOWL_WCF_Production/Service1.svc/GetSpotCrimes',
+   //        {
+   //         method: "POST",
+   //         body: JSON.stringify({
+   //           "UserID":cookie.load('Id'),
+   //           "UserToken":cookie.load('UserToken'),
+   //           "Radius":"0.02",
+   //           // "Lat":this.state.lat1,
+   //           // "Long":this.state.lon1
+   //           "Lat":this.state.lat1,
+   //           "Long":this.state.lon1
+   //
+   //         }),
+   //          headers: new Headers({'content-type': 'application/json'}),
+   //        })
+   //        .then((Response)=> Response.json())
+   //        .then((findresponse)=>{
+   //        //console.log(findresponse);
+   //        this.setState({
+   //               GetSpotCrimesResult:JSON.parse(findresponse.GetSpotCrimesResult).crimes,
+   //
+   //                 })
+   //           console.log(this.state.GetSpotCrimesResult);
+   //
+   //
+   //
+   //     }, (error) => {
+   //          this.setState({ lat1: 'err-latitude', log1: 'err-longitude' })
+   //       })
+   //     })
+   //
+   // }
 }
 
 onMarkerClicked (props, marker, e) {
@@ -62,10 +95,11 @@ onMarkerClicked (props, marker, e) {
         activeMarker: marker,
         showingInfoWindow: true
       });
-     //  console.log("In Marker");
+      // console.log(this.state.showingInfoWindow);
 
 }
-handleMapMount(mapProps, map) {
+handleMapMount(props, map) {
+
     this.map = map;
 
     //log map bounds
@@ -73,13 +107,55 @@ handleMapMount(mapProps, map) {
 
 }
 
+centerMoved(props, map, e) {
 
+  var mapurl = map.rmiUrl ;        //To get Latitude & Longitude
+  var dyn_latitude = mapurl.substring(mapurl.indexOf("@")+1,mapurl.indexOf(","))
+  // console.log(dyn_latitude);
+  var latstring = mapurl.substring(mapurl.indexOf("-"))
+  var dyn_longitude = latstring.substring(latstring.indexOf("-"),latstring.indexOf(","))
+    // console.log(dyn_longitude);
+
+
+        fetch('https://service.howlalarm.com/HOWL_WCF_Production/Service1.svc/GetSpotCrimes',
+        {
+         method: "POST",
+         body: JSON.stringify({
+           "UserID":cookie.load('Id'),
+           "UserToken":cookie.load('UserToken'),
+           "Radius":"0.02",
+           "Lat":dyn_latitude,
+           "Long":dyn_longitude
+
+
+         }),
+          headers: new Headers({'content-type': 'application/json'}),
+        })
+    .then((Response)=> Response.json())
+    .then((findresponse)=>{
+        console.log(findresponse);
+      this.setState({
+               GetSpotCrimesResult:JSON.parse(findresponse.GetSpotCrimesResult).crimes,
+
+                 })
+           // console.log(this.state.GetSpotCrimesResult);
+          // console.log(cookie.load('Latitude'));
+          // console.log(cookie.load('Longitude'));
+
+
+    })
+
+
+
+
+
+}
 componentDidMount()
 {
 
 
   const BaseURL ='https://service.howlalarm.com/HOWL_WCF_Production/Service1.svc/GetSpotCrimes';
-   // 'http://sandbox.howlalarm.com/HOWL_WCF_SANDBOX/Service1.svc/GetSpotCrimes';
+  // const BaseURL = 'http://sandbox.howlalarm.com/HOWL_WCF_SANDBOX/Service1.svc/GetSpotCrimes';
 
 
       fetch(BaseURL,
@@ -92,6 +168,7 @@ componentDidMount()
          "Lat":cookie.load('Latitude'),
          "Long":cookie.load('Longitude')
 
+
        }),
         headers: new Headers({'content-type': 'application/json'}),
       })
@@ -102,12 +179,11 @@ componentDidMount()
              GetSpotCrimesResult:JSON.parse(findresponse.GetSpotCrimesResult).crimes,
 
                })
-        // console.log(this.state.GetSpotCrimesResult);
+        //  console.log(this.state.GetSpotCrimesResult);
         // console.log(cookie.load('Latitude'));
         // console.log(cookie.load('Longitude'));
-        // console.log(  cookie.load('AlertLatitude'));
-        // console.log(cookie.load('AlertLongitude'));
-  
+
+
   })
 
 
@@ -138,8 +214,9 @@ render() {
 
         <Map
 
-            google={google}
-            onClick={this.onMapClicked}
+            google={this.props.google}
+            // onClick={this.onMapClicked}
+            onClick={(e)=>this.onMapClicked()}
 
             initialCenter={{
 
@@ -147,7 +224,8 @@ render() {
               lng: cookie.load('Longitude')
 
             }}
-
+              onDragend={(e)=>this.centerMoved()}
+            onDragend={this.centerMoved}
             zoom={this.state.zoom}
             onReady={this.handleMapMount}
 
@@ -181,6 +259,229 @@ render() {
 
 
             {this.state.GetSpotCrimesResult.map(d => {
+              // var alert_type = d.type ;
+              // switch(alert_type) {
+              //        case 'Arrest':
+              //              return
+              //              <Marker
+              //                   title={'Home Address Location '}
+              //                   // name={
+              //                   //   <div>
+              //                   //     {d.type}
+              //                   //     <div>{d.address}</div>
+              //                   //      <div>{d.date}</div>
+              //                   //   </div>
+              //                   // }
+              //                   onClick={this.onMarkerClicked}
+              //                   position={{lat: d.lat,  lng: d.lon }}
+              //                  icon={{
+              //                           url: "assets/images/Arrest-Icon-Small.png",
+              //                           anchor: new google.maps.Point(32,32),
+              //                           scaledSize: new google.maps.Size(40,40)
+              //                      }}
+              //
+              //
+              //                 />
+              //        case 'Assault':
+              //         return
+              //        <Marker
+              //             title={'Home Address Location '}
+              //             // name={
+              //             //   <div>
+              //             //     {d.type}
+              //             //     <div>{d.address}</div>
+              //             //      <div>{d.date}</div>
+              //             //   </div>
+              //             // }
+              //             onClick={this.onMarkerClicked}
+              //             position={{lat: d.lat,  lng: d.lon }}
+              //            icon={{
+              //                     url: "assets/images/Assult-Icon-Small.png",
+              //                     anchor: new google.maps.Point(32,32),
+              //                     scaledSize: new google.maps.Size(40,40)
+              //                }}
+              //
+              //
+              //           />
+              //        case 'Burglary':
+              //         return
+              //        <Marker
+              //             title={'Home Address Location '}
+              //             name={
+              //               <div>
+              //                 {d.type}
+              //                 <div>{d.address}</div>
+              //                  <div>{d.date}</div>
+              //               </div>
+              //               }
+              //             onClick={this.onMarkerClicked}
+              //               position={{lat: d.lat,  lng: d.lon }}
+              //            icon={{
+              //                     url: "assets/images/Burglary-Icon-Small.png",
+              //                     anchor: new google.maps.Point(32,32),
+              //                     scaledSize: new google.maps.Size(40,40)
+              //                }}
+              //
+              //
+              //           />
+              //        case 'Other':
+              //         return
+              //        <Marker
+              //             title={'Home Address Location '}
+              //             name={
+              //               <div>
+              //                 {d.type}
+              //                 <div>{d.address}</div>
+              //                  <div>{d.date}</div>
+              //               </div>
+              //
+              //                }
+              //             onClick={this.onMarkerClicked}
+              //             position={{lat: d.lat,  lng: d.lon }}
+              //            icon={{
+              //                     url: "assets/images/Other-Icon-Small.png",
+              //                     anchor: new google.maps.Point(32,32),
+              //                     scaledSize: new google.maps.Size(40,40)
+              //                }}
+              //
+              //
+              //           />
+              //        case 'Robbery':
+              //         return
+              //        <Marker
+              //             title={'Home Address Location '}
+              //             name={
+              //               <div>
+              //                 {d.type}
+              //                 <div>{d.address}</div>
+              //                  <div>{d.date}</div>
+              //               </div>
+              //
+              //                }
+              //             onClick={this.onMarkerClicked}
+              //             position={{lat: d.lat,  lng: d.lon }}
+              //            icon={{
+              //                     url: "assets/images/Robbery-Icon-Small.png",
+              //                     anchor: new google.maps.Point(32,32),
+              //                     scaledSize: new google.maps.Size(40,40)
+              //                }}
+              //
+              //
+              //           />
+              //        case 'Shooting':
+              //             return
+              //
+              //             <Marker
+              //                  title={'Home Address Location '}
+              //                  name={
+              //                    <div>
+              //                      {d.type}
+              //                      <div>{d.address}</div>
+              //                       <div>{d.date}</div>
+              //                    </div>
+              //
+              //                     }
+              //                  onClick={this.onMarkerClicked}
+              //                  position={{lat: d.lat,  lng: d.lon }}
+              //                 icon={{
+              //                          url: "assets/images/Shooting-Icon-Small.png",
+              //                          anchor: new google.maps.Point(32,32),
+              //                          scaledSize: new google.maps.Size(40,40)
+              //                     }}
+              //
+              //
+              //                />
+              //       case 'Theft':
+              //             return
+              //             <Marker
+              //                  title={'Home Address Location '}
+              //                  name={
+              //                    <div>
+              //                      {d.type}
+              //                      <div>{d.address}</div>
+              //                       <div>{d.date}</div>
+              //                    </div>
+              //
+              //                     }
+              //                  onClick={this.onMarkerClicked}
+              //                  position={{lat: d.lat,  lng: d.lon }}
+              //                 icon={{
+              //                          url: "assets/images/Theft-Icon-Small.png",
+              //                          anchor: new google.maps.Point(32,32),
+              //                          scaledSize: new google.maps.Size(40,40)
+              //                     }}
+              //
+              //
+              //                />
+              //       case 'Vandalism':
+              //             return
+              //             <Marker
+              //                  title={'Home Address Location '}
+              //                  name={
+              //                    <div>
+              //                      {d.type}
+              //                      <div>{d.address}</div>
+              //                       <div>{d.date}</div>
+              //                    </div>
+              //
+              //                     }
+              //                  onClick={this.onMarkerClicked}
+              //                  position={{lat: d.lat,  lng: d.lon }}
+              //                 icon={{
+              //                          url: "assets/images/Vandalism-Icon-Small.png",
+              //                          anchor: new google.maps.Point(32,32),
+              //                          scaledSize: new google.maps.Size(40,40)
+              //                     }}
+              //
+              //
+              //                />
+              //       case 'Fire':
+              //             return
+              //             <Marker
+              //                  title={'Home Address Location '}
+              //                  name={
+              //                    <div>
+              //                      {d.type}
+              //                      <div>{d.address}</div>
+              //                       <div>{d.date}</div>
+              //                    </div>
+              //
+              //                     }
+              //                  onClick={this.onMarkerClicked}
+              //                  position={{lat: d.lat,  lng: d.lon }}
+              //                 icon={{
+              //                          url: "assets/images/Fire-Arson-Icon-Small.png",
+              //                          anchor: new google.maps.Point(32,32),
+              //                          scaledSize: new google.maps.Size(40,40)
+              //                     }}
+              //
+              //
+              //                />
+              //
+              //       default:
+              //       return
+              //       <Marker
+              //            title={'Home Address Location '}
+              //            name={
+              //              <div>
+              //                {d.type}
+              //                <div>{d.address}</div>
+              //                 <div>{d.date}</div>
+              //              </div>
+              //
+              //               }
+              //            onClick={this.onMarkerClicked}
+              //            position={{lat: d.lat,  lng: d.lon }}
+              //           icon={{
+              //                    url: "assets/images/Fire-Arson-Icon-Small.png",
+              //                    anchor: new google.maps.Point(32,32),
+              //                    scaledSize: new google.maps.Size(40,40)
+              //               }}
+              //
+              //
+              //          />
+              //
+              //        }
 
            if(d.type === "Arrest"){
                return (
